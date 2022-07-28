ee0b9b116407060fb109c9d84bb8403f85908128
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-07-28 09:38:51 +0100

    - monitor memory usage

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
-﻿using Maybe.BloomFilter;
+﻿using System.Diagnostics;
+using Maybe.BloomFilter;
 
 const int MaxItems = 1000000;
 const double AcceptableErrorRate = 0.01;
 
+var proc = Process.GetCurrentProcess();
+
 var clashes = GetClashes(MaxItems, AcceptableErrorRate);
 
+proc.Refresh();
+var maxMem = proc.PeakWorkingSet64;
+
 Console.WriteLine($"MaxItems            = {MaxItems}");
-Console.WriteLine($"AcceptableErrorRate = {AcceptableErrorRate}");
+Console.WriteLine($"AcceptableErrorRate = {AcceptableErrorRate * 100} %");
 Console.WriteLine($"MaxErrors           = {MaxItems * AcceptableErrorRate}");
+Console.WriteLine($"PeakWorkingSet64    = {maxMem / (1024 * 1024)} MB");
 Console.WriteLine($"Clashes [{clashes.Count}]:");
 foreach (var clash in clashes)
 {
