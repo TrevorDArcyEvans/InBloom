20cb6c4649b42e671dabb200a3ef89b388bf46db
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-07-28 09:14:57 +0100

    - initial version

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Maybe.BloomFilter;
+
+const int MaxItems = 1000000;
+const double AcceptableErrorRate = 0.01;
+
+var filter = new BloomFilter<Guid>(MaxItems, AcceptableErrorRate);
+var clashes = TestFilter(MaxItems, filter);
+
+Console.WriteLine($"MaxItems            = {MaxItems}");
+Console.WriteLine($"AcceptableErrorRate = {AcceptableErrorRate}");
+Console.WriteLine($"Clashes [{clashes.Count}]:");
+foreach (var clash in clashes)
+{
+  Console.WriteLine($"  {clash}");
+}
+
+List<int> TestFilter(int maxItems, BloomFilter<Guid> bloomFilter)
+{
+  var retval = new List<int>();
+  foreach (var i in Enumerable.Range(0, maxItems))
+  {
+    if (bloomFilter.AddAndCheck(Guid.NewGuid()))
+    {
+      retval.Add(i);
+    }
+  }
+
+  return retval;
+}
