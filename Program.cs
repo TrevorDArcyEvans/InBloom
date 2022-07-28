1e6f54d60a5f09514d888cf05bc408998ca3f388
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-07-28 09:21:56 +0100

    - refactored

--- Program.cs
+++ Program.cs
@@ -3,23 +3,24 @@
 const int MaxItems = 1000000;
 const double AcceptableErrorRate = 0.01;
 
-var filter = new BloomFilter<Guid>(MaxItems, AcceptableErrorRate);
-var clashes = TestFilter(MaxItems, filter);
+var clashes = GetClashes(MaxItems, AcceptableErrorRate);
 
 Console.WriteLine($"MaxItems            = {MaxItems}");
 Console.WriteLine($"AcceptableErrorRate = {AcceptableErrorRate}");
+Console.WriteLine($"MaxErrors           = {MaxItems * AcceptableErrorRate}");
 Console.WriteLine($"Clashes [{clashes.Count}]:");
 foreach (var clash in clashes)
 {
   Console.WriteLine($"  {clash}");
 }
 
-List<int> TestFilter(int maxItems, BloomFilter<Guid> bloomFilter)
+List<int> GetClashes(int maxItems, double acceptableErrorRate)
 {
+  var filter = new BloomFilter<Guid>(maxItems, acceptableErrorRate);
   var retval = new List<int>();
   foreach (var i in Enumerable.Range(0, maxItems))
   {
-    if (bloomFilter.AddAndCheck(Guid.NewGuid()))
+    if (filter.AddAndCheck(Guid.NewGuid()))
     {
       retval.Add(i);
     }
