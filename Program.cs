40edf8f3bf0fbbd3a04336ce486d573bed23cfe4
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-07-28 09:56:57 +0100

    - add commandline support for specifying number of items and error rate

--- Program.cs
+++ Program.cs
@@ -1,37 +1,59 @@
-﻿using System.Diagnostics;
+﻿namespace InBloom;
+
+using System.Diagnostics;
 using Maybe.BloomFilter;
+using CommandLine;
 
-const int MaxItems = 1000000;
-const double AcceptableErrorRate = 0.01;
-
-var proc = Process.GetCurrentProcess();
-
-var clashes = GetClashes(MaxItems, AcceptableErrorRate);
-
-proc.Refresh();
-var maxMem = proc.PeakWorkingSet64;
-
-Console.WriteLine($"MaxItems            = {MaxItems}");
-Console.WriteLine($"AcceptableErrorRate = {AcceptableErrorRate * 100} %");
-Console.WriteLine($"MaxErrors           = {MaxItems * AcceptableErrorRate}");
-Console.WriteLine($"PeakWorkingSet64    = {maxMem / (1024 * 1024)} MB");
-Console.WriteLine($"Clashes [{clashes.Count}]:");
-foreach (var clash in clashes)
+public static class Program
 {
-  Console.WriteLine($"  {clash}");
-}
-
-List<int> GetClashes(int maxItems, double acceptableErrorRate)
-{
-  var filter = new BloomFilter<Guid>(maxItems, acceptableErrorRate);
-  var retval = new List<int>();
-  foreach (var i in Enumerable.Range(0, maxItems))
+  public static void Main(string[] args)
   {
-    if (filter.AddAndCheck(Guid.NewGuid()))
-    {
-      retval.Add(i);
-    }
+    Parser.Default.ParseArguments<Options>(args)
+      .WithParsed<Options>(opt =>
+      {
+        var numItems = opt.NumItems;
+        var acceptableErrorRate = opt.AcceptableErrorRate;
+
+        var proc = Process.GetCurrentProcess();
+
+        var clashes = GetClashes(numItems, acceptableErrorRate);
+
+        proc.Refresh();
+        var maxMem = proc.PeakWorkingSet64;
+
+        Console.WriteLine($"NumItems            = {numItems}");
+        Console.WriteLine($"AcceptableErrorRate = {acceptableErrorRate * 100} %");
+        Console.WriteLine($"MaxErrors           = {numItems * acceptableErrorRate}");
+        Console.WriteLine($"PeakWorkingSet64    = {maxMem / (1024 * 1024)} MB");
+        Console.WriteLine($"Clashes [{clashes.Count}]:");
+        foreach (var clash in clashes)
+        {
+          Console.WriteLine($"  {clash}");
+        }
+      });
   }
 
-  return retval;
+  private static List<int> GetClashes(int numItems, double acceptableErrorRate)
+  {
+    var filter = new BloomFilter<Guid>(numItems, acceptableErrorRate);
+    var retval = new List<int>();
+    foreach (var i in Enumerable.Range(0, numItems))
+    {
+      if (filter.AddAndCheck(Guid.NewGuid()))
+      {
+        retval.Add(i);
+      }
+    }
+
+    return retval;
+  }
+
+  public class Options
+  {
+    [Option('n', "numItems", Required = false, Default = 1000000, HelpText = "Number of items in data")]
+    public int NumItems { get; set; }
+
+    [Option('e', "errorRate", Required = false, Default = 0.01, HelpText = "Acceptable error rate (0 .. 1)")]
+    public double AcceptableErrorRate { get; set; }
+  }
 }
